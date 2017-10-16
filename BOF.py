1690567daafb353bc13406007b6506f0304722b3
devesh <deveshbajaj59@gmail.com>
2017-10-16 19:22:35 +0530

    simple bag of word for small data

---
+++ BOF.py
@@ -0,0 +1,36 @@
+from collections import Counter
+import pandas
+import numpy
+headlines = [
+    "PretzelBros, airbnb for people who like pretzels, raises $2 million",
+    "Top 10 reasons why Go is better than whatever language you use.",
+    "Why working at apple stole my soul (I still love it though)",
+    "80 things I think you should do immediately if you use python.",
+    "Show HN: carjack.me -- Uber meets GTA"
+]
+
+
+unique_words = list(set(" ".join(headlines).split(" ")))
+print(unique_words)
+
+def make_matrix(headlines, vocab):
+    matrix = []
+    for headline in headlines:
+        print(headline)
+        counter = Counter(headline)
+        print(counter)
+
+        # Turn the dictionary into a matrix row using the vocab.
+        row = [counter.get(w, 0) for w in vocab]
+        print(row)
+        matrix.append(row)
+    df = pandas.DataFrame(matrix)
+    df.columns = unique_words
+    return df
+
+x=make_matrix(headlines, unique_words)
+print(type(x))
+print(x)
+
+x.to_csv("file_path.csv")
+print("done")
