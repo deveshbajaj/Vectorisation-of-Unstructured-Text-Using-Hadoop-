63d9149b0f925951756b6cea2efde8b51e3caeb8
devesh <deveshbajaj59@gmail.com>
2017-11-06 20:00:24 +0530

    find unique words

---
+++ getter.py
@@ -0,0 +1,31 @@
+import sys
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+stop = set(stopwords.words('english'))
+
+text = ''
+ 
+#print(stop)
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    
+
+    line = re.sub(r'[^\w\s]','',line)
+    #text = text +line+' '
+
+    word_tokens = word_tokenize(line)
+    filtered_sentence = [w for w in word_tokens if not w in stop]
+    final = ' '.join(filtered_sentence)
+    print(final)
+
+"""unique_words = list(set(text.split(" ")))
+print(len(unique_words))
+final = ' '.join(unique_words)
+f = open('words.txt','w')
+f.write(final)
+f.close()
+#print(unique_words)
+print("done")"""
